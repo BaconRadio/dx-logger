2c6d341af2551f5728f2a88948f5d827419f205b
CJ <cjtheham@protonmail.com>
2022-03-26 22:57:27 -0500

    move weird vars to dotenv

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -6,7 +6,7 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { LogbooksModule } from './logbooks/logbooks.module';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://server:password@localhost:27017/dx-logger-db'), LogbooksModule],
+  imports: [MongooseModule.forRoot(`mongodb://${process.env.DB_IP}:${process.env.DP_PORT}/dx-logger-db`), LogbooksModule],
   controllers: [AppController],
   providers: [AppService],
 })
