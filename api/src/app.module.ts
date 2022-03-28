13e8ca36d3d5304bd0d5f3b155a1508b5a4d1e7c
CJ <cjtheham@protonmail.com>
2022-03-28 11:52:10 -0500

    logs schema

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 
 import { MongooseModule } from '@nestjs/mongoose';
 import { LogbooksModule } from './logbooks/logbooks.module';
+import { LogsModule } from './logs/logs.module';
 
 @Module({
-  imports: [MongooseModule.forRoot(`mongodb://${process.env.DB_IP}:${process.env.DP_PORT}/dx-logger-db`), LogbooksModule],
+  imports: [MongooseModule.forRoot(`mongodb://${process.env.DB_IP}:${process.env.DP_PORT}/dx-logger-db`), LogbooksModule, LogsModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ api/src/logs/schemas/log.schema.ts
@@ -0,0 +1,143 @@
+import { Prop, Schema } from '@nestjs/mongoose';
+import { Logbook } from 'src/logbooks/schemas/logbook.schema';
+
+@Schema()
+export class Log {
+  // TODO - Required fields
+
+  /* 
+  
+  Connections
+
+  */
+
+  @Prop()
+  logbook: Logbook;
+
+  // TODO
+  @Prop()
+  station: Station;
+
+  /*
+
+  Basic Details
+
+  */
+
+  @Prop()
+  contest: string;
+
+  @Prop()
+  myCallsign: string;
+
+  @Prop()
+  myGrid: string;
+
+  @Prop()
+  theirCall: string;
+
+  @Prop()
+  rstSent: number;
+
+  @Prop()
+  rstRcvd: number;
+
+  @Prop()
+  startTime: Date;
+
+  @Prop()
+  endTime: Date;
+
+  // TODO - long string?
+  @Prop()
+  notes: string;
+
+  @Prop()
+  theirName: string;
+
+  @Prop()
+  theirQTH: string;
+
+  @Prop()
+  theirState: string;
+
+  @Prop()
+  theirCounty: string;
+
+  @Prop()
+  theirGrid: string;
+
+  // Note - in KHZ
+  @Prop()
+  frequency: number;
+
+  // May be able to auto gen based on freq
+  @Prop()
+  band: number;
+
+  @Prop()
+  power: number; // in watts
+
+  // TODO - enum
+  @Prop()
+  mode: string;
+
+  /* 
+  
+  Contest Specific Fields
+  
+  */
+
+  @Prop()
+  myExch: string;
+
+  @Prop()
+  theirExch: string;
+
+  // for multi ops
+  @Prop()
+  operator: string;
+
+  @Prop()
+  isMultiplier: boolean;
+
+  @Prop()
+  wasRunning: boolean;
+
+  /*
+
+  Satellites
+
+  TODO - validate sat names + freq
+
+  */
+
+  @Prop()
+  satellite: string;
+
+  /*
+
+  QSL Information
+
+  Plan is to sync against misc platforms and update these fields.
+
+  */
+
+  @Prop()
+  qslSentLOTW: boolean;
+
+  @Prop()
+  qslSentQRZ: boolean;
+
+  @Prop()
+  qslRcvdLOTW: boolean;
+
+  @Prop()
+  qslRcvdQRZ: boolean;
+
+  @Prop()
+  qslSentPhysical: boolean;
+
+  @Prop()
+  qslRcvdPhysical: boolean;
+}
